393bb746580f29246c0b51ca4d7711e226f919a6
fuegoai <fuego@incube8.sg>
2024-03-10 19:56:02 +0800

    Update cart price

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -6,13 +6,7 @@ import useFromStore from "../../hooks/useFromStore";
 
 function Cart() {
   const cart = useFromStore(useCartStore, (state) => state.cart);
-  let total = 0;
-  if (cart) {
-    total = cart.reduce(
-      (acc, product) => acc + product.price * (product.quantity as number),
-      0
-    );
-  }
+  const totalPrice = useFromStore(useCartStore, (state) => state.totalPrice);
 
   return (
     <section>
@@ -24,7 +18,9 @@ function Cart() {
       </ul>
       <div className="flex justify-between items-center mt-4">
         <span className="text-lg font-bold">Total:</span>
-        <span className="text-xl font-bold">${total.toFixed(2)}</span>
+        <span className="text-xl font-bold">
+          ${Number(totalPrice).toFixed(2)}
+        </span>
       </div>
     </section>
   );
